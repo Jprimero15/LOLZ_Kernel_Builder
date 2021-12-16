812997a3b8c348084a556c668814e26870d1181f
Joshua Primero <jprimero155@gmail.com>
2021-12-17 00:32:27 +0800

    Boi

--- lolzkernel.sh
+++ lolzkernel.sh
@@ -7,9 +7,9 @@ export DEBIAN_FRONTEND=noninteractive
 ZDIR="$(pwd)"
 
 # clone scripts boi
-git clone https://Jprimero15:$GITHUB_TOKEN@github.com/Jprimero15/lolz-clang -b new --depth=1 $ZDIR/builder
+git clone https://Jprimero15:$GITHUB_TOKEN@github.com/Jprimero15/lolz-clang-build -b new --depth=1 $ZDIR/builder
 
 # LOLZ CLANG Builder Script
-cd $ZDIR/builder && bash $ZDIR/builder/build_lolz.sh
+cd $ZDIR/builder && bash build_lolz.sh
 
 # End of Script
