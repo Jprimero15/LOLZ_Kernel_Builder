541f09b9aa5b50aa20c95b839bb89f66455ddebf
Joshua Primero <jprimero155@gmail.com>
2021-12-19 16:35:42 +0800

    Note 3 hlte support

--- lolzkernel.sh
+++ lolzkernel.sh
@@ -13,7 +13,7 @@ git config --global user.email "jprimero155@gmail.com"
 # Inlined function to post a message
 export BOT_MSG_URL="https://api.telegram.org/bot$TG_BOT_TOKEN/sendMessage"
 function tg_post_msg() {
-    curl -s -X POST "$BOT_MSG_URL" -d chat_id="-1001366235952" \
+    curl -s -X POST "$BOT_MSG_URL" -d chat_id="-1001222358827" \
         -d "disable_web_page_preview=true" \
         -d "parse_mode=html" \
         -d text="$1"
@@ -21,24 +21,13 @@ function tg_post_msg() {
 
 # Send a notificaton to TG
 tg_post_msg " <b>LOLZ KERNEL Compilation Started. </b>
-       
-<b>Some Informations about Build Types: </b>
-<b>BUILT-IN WLAN: <code>This is for Android 10,11 and 12 AOSP Based Custom ROMs, e.g. CrDroid.</code></b>
-<b>MODULE_WLAN: <code>This is for Android 10 MIUI Based ROMs and GSI. </code></b>
 
-<b>For more questions just message on this group and mention Jprimero15 (DO NOT DIRECT MESSAGE HIM)</b> "
-
-git clone https://Jprimero15:$GITHUB_TOKEN@github.com/Jprimero15/lolzbuilder -b master $LOLZ_DIR/builder 
-
-git clone https://github.com/Jprimero15/lolz_kernel_redmi8 -b v14_up $LOLZ_DIR/lolz
+git clone https://github.com/Jprimero15/lolz_rebase -b builder --depth=1 $LOLZ_DIR/lolz
 
 git clone https://github.com/Jprimero15/lolz-clang -b main --depth=1 $LOLZ_DIR/lolz/clang14
 
-# for AOSP based Script
-cd $LOLZ_DIR/lolz && bash $LOLZ_DIR/builder/lolzbuilder.sh
-
-# for MIUI based Script
-cd $LOLZ_DIR/lolz && bash $LOLZ_DIR/builder/lolzbuilder_miui.sh
+# note 3 based Script
+cd $LOLZ_DIR/lolz && bash builder.sh
 
 # Send a notificaton to TG
 tg_post_msg "<b>LOLZ KERNEL Compilation Completed</b>"
