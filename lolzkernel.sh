807e0c4f8ac866e3ce3c6f3903a35af6494480d7
Joshua Primero <jprimero155@gmail.com>
2021-12-21 09:50:30 +0800

    Build all at once
    * nuke telegram messages since we moved it to main builder.

--- lolzkernel.sh
+++ lolzkernel.sh
@@ -10,18 +10,6 @@ LOLZ_DIR="$(pwd)"
 git config --global user.name "Jprimero15"
 git config --global user.email "jprimero155@gmail.com"
 
-# Inlined function to post a message
-export BOT_MSG_URL="https://api.telegram.org/bot$TG_BOT_TOKEN/sendMessage"
-function tg_post_msg() {
-    curl -s -X POST "$BOT_MSG_URL" -d chat_id="-1001222358827" \
-        -d "disable_web_page_preview=true" \
-        -d "parse_mode=html" \
-        -d text="$1"
-}
-
-# Send a notificaton to TG
-tg_post_msg " <b>LOLZ KERNEL Compilation Started. </b>"
-
 git clone https://github.com/Jprimero15/lolz_rebase -b builder --depth=1 $LOLZ_DIR/lolz
 
 git clone https://github.com/Jprimero15/lolz-clang -b main --depth=1 $LOLZ_DIR/lolz/clang14
@@ -29,7 +17,10 @@ git clone https://github.com/Jprimero15/lolz-clang -b main --depth=1 $LOLZ_DIR/l
 # note 3 based Script
 cd $LOLZ_DIR/lolz && bash builder.sh
 
-# Send a notificaton to TG
-tg_post_msg "<b>LOLZ KERNEL Compilation Completed</b>" 
+cd $LOLZ_DIR/lolz && bash builder.sh hltechn
+
+cd $LOLZ_DIR/lolz && bash builder.sh hltekor
+
+cd $LOLZ_DIR/lolz && bash builder.sh hltetmo
 
 # End of Script
