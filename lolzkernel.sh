6dc7db84fb0ecf59183f056742f16a337e6f807a
Jprimero15 <jprimero15@aospa.co>
2024-10-15 06:02:13 +0800

    vauxite_new ksu test?

--- lolzkernel.sh
+++ lolzkernel.sh
@@ -28,12 +28,12 @@ tg_post_msg " <b>LOLZ KERNEL Compilation Started. </b>
 
 <b>For more questions just message on this group and mention Jprimero15 (DO NOT DIRECT MESSAGE HIM)</b> "
 
-git clone https://github.com/AOSPA/android_kernel_xiaomi_sdm439 -b vauxite --depth=1 $LOLZ_DIR/lolz
+git clone https://github.com/Jprimero15/android_kernel_xiaomi_sdm439-4.19 -b vauxite_new --depth=1 $LOLZ_DIR/lolz
 
 git clone https://Jprimero15:$GITHUB_TOKEN@github.com/Jprimero15/lolzbuilder -b master $LOLZ_DIR/lolz/builder
 
 # for AOSP based Script
-cd $LOLZ_DIR/lolz && bash $LOLZ_DIR/lolz/builder/new_lolzbuilder.sh 
+cd $LOLZ_DIR/lolz && bash $LOLZ_DIR/lolz/builder/new_lolzbuilder.sh ksu
 
 # Send a notificaton to TG
 tg_post_msg "<b>LOLZ KERNEL Compilation Completed</b>"
