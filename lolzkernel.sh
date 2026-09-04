c56df2b004b899f93d359d07687e4e1b04da99eb
Jprimero15 <jprimero15@aospa.co>
2026-09-05 07:27:35 +0800

    Some changes on script and use proton clang

--- lolzkernel.sh
+++ lolzkernel.sh
@@ -4,23 +4,26 @@ set -euo pipefail
 
 export DEBIAN_FRONTEND=noninteractive
 
-LOLZ_DIR="$(pwd)"
+LDIR="$(pwd)/lolz"
 
 # Github info
 git config --global user.name "Jprimero15"
 git config --global user.email "jprimero15@aospa.co"
 
-git clone https://github.com/Jprimero15/lolz_rebase -b v18-next --depth=1 $LOLZ_DIR/lolz
+git clone https://github.com/Jprimero15/lolz_rebase -b v18-next --depth=1 $LDIR
 
-git clone https://github.com/curbthepain/linaro-arm-eabi-4.9 -b master --depth=1 $LOLZ_DIR/lolz/gcc
+git clone https://github.com/kdrag0n/proton-clang -b master --depth=1 $LDIR/clang15
+
+rm -rf $LDIR/KernelSU # always remove
+git clone https://github.com/backslashxx/KernelSU -b master $LDIR/KernelSU
 
 # note 3 based Script
-cd $LOLZ_DIR/lolz && bash builder.sh
+cd $LDIR && bash builder.sh
 
-#cd $LOLZ_DIR/lolz && bash builder.sh hltechn
+#cd $LDIR && bash builder.sh hltechn
 
-#cd $LOLZ_DIR/lolz && bash builder.sh hltekor
+#cd $LDIR && bash builder.sh hltekor
 
-#cd $LOLZ_DIR/lolz && bash builder.sh hltetmo
+#cd $LDIR && bash builder.sh hltetmo
 
 # End of Script
