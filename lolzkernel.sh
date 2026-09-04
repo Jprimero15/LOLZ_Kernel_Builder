e8f58d7749b654065267aba36c8dd52d5af02c53
Jprimero15 <jprimero15@aospa.co>
2026-09-04 18:31:29 +0800

    test gcc ?
     test build hlte for now

--- lolzkernel.sh
+++ lolzkernel.sh
@@ -10,17 +10,17 @@ LOLZ_DIR="$(pwd)"
 git config --global user.name "Jprimero15"
 git config --global user.email "jprimero15@aospa.co"
 
-git clone https://github.com/Jprimero15/lolz_rebase -b v18 --depth=1 $LOLZ_DIR/lolz
+git clone https://github.com/Jprimero15/lolz_rebase -b v18-next --depth=1 $LOLZ_DIR/lolz
 
-git clone https://github.com/Jprimero15/lolz-clang -b main --depth=1 $LOLZ_DIR/lolz/clang14
+git clone https://github.com/Dhruvgera/EvaGCC-arm -b master --depth=1 $LOLZ_DIR/lolz/gcc10
 
 # note 3 based Script
-cd $LOLZ_DIR/lolz && bash builder.sh
+cd $LOLZ_DIR/lolz && bash builder.sh --gcc
 
-cd $LOLZ_DIR/lolz && bash builder.sh hltechn
+#cd $LOLZ_DIR/lolz && bash builder.sh hltechn
 
-cd $LOLZ_DIR/lolz && bash builder.sh hltekor
+#cd $LOLZ_DIR/lolz && bash builder.sh hltekor
 
-cd $LOLZ_DIR/lolz && bash builder.sh hltetmo
+#cd $LOLZ_DIR/lolz && bash builder.sh hltetmo
 
 # End of Script
