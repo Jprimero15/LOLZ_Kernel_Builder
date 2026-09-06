be208ceadfd51ac1fa46b196a521e1a98359a2b2
Jprimero15 <jprimero15@aospa.co>
2026-09-06 14:35:45 +0800

    clang 23?

--- lolzkernel.sh
+++ lolzkernel.sh
@@ -12,9 +12,7 @@ git config --global user.email "jprimero15@aospa.co"
 
 git clone https://github.com/Jprimero15/lolz_rebase -b v18-next --depth=1 $LOLZDIR
 
-git clone https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r596125 -b 16.0 $LOLZDIR/clang_tc
-cd $LOLZDIR/clang_tc && git lfs install && git lfs pull && cd $LOLZDIR
-git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 -b lineage-19.1 --depth=1 $LOLZDIR/gcc_tc
+git clone https://github.com/H33CKER/clang -b main --depth=1 $LOLZDIR/gcc_tc
 
 rm -rf $LOLZDIR/KernelSU # always remove
 git clone https://github.com/backslashxx/KernelSU -b master $LOLZDIR/KernelSU
