e55402ff728ab01cdc7ffea72afbf2f401b4c080
Jprimero15 <jprimero15@aospa.co>
2026-09-07 15:01:00 +0800

    test sdclang 19

--- lolzkernel.sh
+++ lolzkernel.sh
@@ -14,7 +14,7 @@ git config --global user.email "jprimero15@aospa.co"
 
 git clone https://github.com/Jprimero15/lolz_rebase -b v18-next --depth=1 $LOLZDIR
 
-mkdir -p $LOLZDIR/clang_tc && curl -L "https://github.com/ZyCromerZ/Clang/releases/download/15.0.7-20230204-release/Clang-15.0.7-20230204.tar.gz" | tar -xz -C $LOLZDIR/clang_tc
+git clone https://github.com/ThankYouMario/proprietary_vendor_qcom_sdclang -b 19 --depth=1 $LOLZDIR/clang_tc 
 
 rm -rf $LOLZDIR/KernelSU # always remove
 git clone https://github.com/backslashxx/KernelSU -b v3.3.0-18 $LOLZDIR/KernelSU
